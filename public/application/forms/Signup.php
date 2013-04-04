c1db2edd930e379c0f91f3df92232c96912a2715
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-03 21:20:50 -0300

    public/application/forms/Signup.php: Determine proper base-URL for captcha image based on the URL via which the form is accessed (again, so it need not matter whether we're in production or development mode).

--- public/application/forms/Signup.php
+++ public/application/forms/Signup.php
@@ -1,8 +1,15 @@
 <?php
 
+require_once 'my-php-libs/url.php';                     # constructUrlFromRelativeLocation
+require_once 'my-php-libs/webapp/current-request.php';  # CurrentRequest\...
+
+use \MyPHPLibs\URL, \MyPHPLibs\Webapp\CurrentRequest;
+
 class Application_Form_Signup extends Zend_Form {
 
   public function init() {
+    $thisURL = CurrentRequest\getURL();
+    $captchasDirURL = URL\constructUrlFromRelativeLocation($thisURL, '/images/captcha');
     $captcha = new Zend_Form_Element_Captcha('captcha', array(
       'captcha' => array(
         'captcha' => 'Image',
@@ -11,7 +18,7 @@ class Application_Form_Signup extends Zend_Form {
         'width' => '100',
         'height' => '41',
         'font' => 'fonts/arial.ttf',
-        'imgUrl' => PATH .'images/captcha',
+        'imgUrl' => $captchasDirURL,
         'imgDir' => 'images/captcha')
     ));
     $captcha->setDecorators(array(
