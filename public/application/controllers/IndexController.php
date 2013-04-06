6c4ea150607c81da9bf80f4603b6a7b66b893bb1
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-06 14:32:02 -0300

    public/application/controllers/IndexController.php: Clean up this file (fix wonky formatting, remove useless comments, etc), and implement HTTP redirects so as to avoid a hard-coded HTTP host (this so it still functions properly when running on my local dev machine).

--- public/application/controllers/IndexController.php
+++ public/application/controllers/IndexController.php
@@ -1,33 +1,35 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
-{
+require_once 'my-php-libs/url.php';                     # constructUrlFromRelativeLocation
+require_once 'my-php-libs/webapp/current-request.php';  # CurrentRequest\getURL
 
-	private $auth;
+use \MyPHPLibs\URL, \MyPHPLibs\Webapp\CurrentRequest;
 
-    public function init()
-    {
-        /* Initialize action controller here */
-        
-        $this->auth = Zend_Auth::getInstance();
-		
-		if(!$this->auth->hasIdentity())
-			$this->_redirect(PATH.'signin/index/');
-		
-		if($this->auth->getIdentity()->role == 'admin'){
-			$this->_redirect(PATH.'admin/index/');
-		}
-		
-		$this->_redirect(PATH.'dashboard/index/');
+class IndexController extends Zend_Controller_Action {
+
+  private $auth;
+
+  public function init() {
+    $this->auth = Zend_Auth::getInstance();
+    if (!$this->auth->hasIdentity()) {
+      $this->relativeRedirect('/signin/index/');
     }
-
-    public function indexAction()
-    {
-        // action body
+    if ($this->auth->getIdentity()->role == 'admin') {
+      $this->relativeRedirect('/admin/index/');
     }
+    $this->relativeRedirect('/dashboard/index/');
+  }
 
+  public function indexAction() { }
+
+  protected function relativeRedirect($path, $code = 302) {
+    $host = $_SERVER['HTTP_HOST'];
+    if (substr($path, 0, strlen($host) == $host)) {
+      throw new Exception("redirect should be called with a relative or absolute path, " .
+        "without the HTTP_HOST as a prefix");
+    }
+    //throw new DoRedirect($path, $code);
+    $url = URL\constructUrlFromRelativeLocation(CurrentRequest\getURL(), $path);
+    $this->_redirect($url);
+  }
 }
-
-
-
-?>
