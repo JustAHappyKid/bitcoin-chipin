bef0078d6594ed85c0337cacbf1651f86966af64
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-06 15:35:18 -0300

    public/application/models/Register.php: Tidying up...

--- public/application/models/Register.php
+++ public/application/models/Register.php
@@ -1,49 +1,36 @@
 <?php
 
-class Application_Model_Register
-{
-	private $_dbTable;
-	
-	public function __construct()
-	{
-		$this->_dbTable = Zend_Db_Table::getDefaultAdapter();
-	}
-	
-	public function createUser($data)
-	{
-		$this->_dbTable->insert('users', $data);
-	}
-	
-	public function updateUsersPassword($data, $id)
-	{
-		$this->_dbTable->update('users', $data, $id);
-	}
-	
-	public function ifExpired($code, $user_id){
-		$select = $this->_dbTable
-			->select()
-			->from('reset_password')
-			->where('user_id = "'.$user_id.'" AND code="'.$code.'" AND expires >= NOW() AND status = 1')
-			->order('id DESC')
-			->limit('1');
-		
-		$result = $this->_dbTable->fetchAll($select);
-		if(!isset($result[0])){
-			return FALSE;
-		}
-		else{
-			return $result[0];
-		}
-	}
-	
-	public function removeConfirmationLink($code, $user_id)
-	{
-		$this->_dbTable->delete('reset_password', array(
-			'user_id = ?' => $user_id,
-			'code = ?' => $code
-		));
-	}
-	
-}
+class Application_Model_Register {
 
-?>
+  private $_dbTable;
+
+  public function __construct() {
+    $this->_dbTable = Zend_Db_Table::getDefaultAdapter();
+  }
+
+  public function createUser($data) {
+    $this->_dbTable->insert('users', $data);
+  }
+
+  public function updateUsersPassword($data, $id) {
+    $this->_dbTable->update('users', $data, $id);
+  }
+
+  public function ifExpired($code, $userID) {
+    $select = $this->_dbTable
+      ->select()
+      ->from('reset_password')
+      ->where('user_id = "'.$userID.'" AND code="'.$code.'" AND expires >= NOW() AND status = 1')
+      ->order('id DESC')
+      ->limit('1');
+    $result = $this->_dbTable->fetchAll($select);
+    return isset($result[0]);
+  }
+
+  public function removeConfirmationLink($code, $userID) {
+    $this->_dbTable->delete('reset_password', array(
+      'user_id = ?' => $userID,
+      'code = ?' => $code
+    ));
+  }
+}
