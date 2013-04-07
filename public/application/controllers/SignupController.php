e6bb6c8f12f6d190696362a0bfb6d26ce41d8603
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-06 22:43:54 -0300

    public/application/controllers/SignupController.php: And more tidying.

--- public/application/controllers/SignupController.php
+++ public/application/controllers/SignupController.php
@@ -2,81 +2,74 @@
 
 class SignupController extends Zend_Controller_Action {
 
-    public function indexAction() {
-        // action body
-        $this->_helper->layout->setLayout('layout1');
+  public function indexAction() {
+    $this->_helper->layout->setLayout('layout1');
+    $form = new Application_Form_Signup();
+    $this->view->form = $form;
+    if($this->_getParam("captcha", "") == "error")
+        $this->view->captcha = true;
+  }
+
+  public function signupAction() {
+    $this->_helper->layout->disableLayout();
+
+    $username = $this->_getParam("username");
+    //$password = md5($this->_getParam("password"));
+
+    ////////////////////////////////////////////
+    ////////////////////////////////////////////
+    $bcrypt = new Application_Model_Bcrypt(20);
+    $password = $bcrypt->hash($this->_getParam("password"));
+    ////////////////////////////////////////////
+    ////////////////////////////////////////////
+
+    $email = $this->_getParam("email");
+    /*
+    $captcha = $this->_getParam('captcha');
+    $form = new Application_Form_Signup();
+    if(!$form->isValid($captcha))
+      $this->_redirect(PATH.'signup/index/captcha/error/');
+    */
+    if($username != '' && $password != '') {
+
+      $register = new Application_Model_Register();
+      $register->createUser(array(
+        'username' => $username,
+        'password' => $password,
+        'email' => $email
+      ));
+      
+      $dbAdapter = Zend_Db_Table::getDefaultAdapter();
+      $authAdapter = new Zend_Auth_Adapter_DbTable($dbAdapter);
+      
+      $authAdapter->setTableName("users");
+      $authAdapter->setIdentityColumn("username");
+      $authAdapter->setCredentialColumn("password");
+      
+      
+      $authAdapter->setIdentity($username);
+      $authAdapter->setCredential($password);
+      
+      $auth = Zend_Auth::getInstance();
+      $result = $auth->authenticate($authAdapter);
+      
+      if($result->isValid()) {
+        $identity = $authAdapter->getResultRowObject();
         
-        $form = new Application_Form_Signup();
-        $this->view->form = $form;
+        $authStorage = $auth->getStorage();
+        $authStorage->write($identity);
 
-        if($this->_getParam("captcha", "") == "error")
-            $this->view->captcha = true;
-    }
-
-    public function signupAction() {
-        $this->_helper->layout->disableLayout();
-
-        $username = $this->_getParam("username");
-        //$password = md5($this->_getParam("password"));
-
-        ////////////////////////////////////////////
-        ////////////////////////////////////////////
-        $bcrypt = new Application_Model_Bcrypt(20);
-        $password = $bcrypt->hash($this->_getParam("password"));
-        ////////////////////////////////////////////
-        ////////////////////////////////////////////
-
-        $email = $this->_getParam("email");
-        /*
-        $captcha = $this->_getParam('captcha');
-        $form = new Application_Form_Signup();
-        if(!$form->isValid($captcha))
-          $this->_redirect(PATH.'signup/index/captcha/error/');
-        */
-        if($username != '' && $password != '')
-        {
-          $register = new Application_Model_Register();
-          $register->createUser(array(
-            'username' => $username,
-            'password' => $password,
-            'email' => $email
-          ));
-          
-          $dbAdapter = Zend_Db_Table::getDefaultAdapter();
-          $authAdapter = new Zend_Auth_Adapter_DbTable($dbAdapter);
-          
-          $authAdapter->setTableName("users");
-          $authAdapter->setIdentityColumn("username");
-          $authAdapter->setCredentialColumn("password");
-          
-          
-          $authAdapter->setIdentity($username);
-          $authAdapter->setCredential($password);
-          
-          $auth = Zend_Auth::getInstance();
-          $result = $auth->authenticate($authAdapter);
-          
-          if($result->isValid())
-          {
-            $identity = $authAdapter->getResultRowObject();
-            
-            $authStorage = $auth->getStorage();
-            $authStorage->write($identity);
-
-            if($identity->role == 'admin'){
-              $this->_redirect(PATH.'admin/index/');
-            }
-
-            $this->_redirect(PATH."dashboard/index/");
-          }
-          else
-          {
-            $this->_redirect(PATH.'signin/index/');
-          }
+        if($identity->role == 'admin') {
+          $this->_redirect(PATH.'admin/index/');
         }
-        else
-        {
-          $this->_redirect(PATH.'signin/index/');
-        }
+
+        $this->_redirect(PATH."dashboard/index/");
+      } else {
+        $this->_redirect(PATH.'signin/index/');
+      }
     }
+    else {
+      $this->_redirect(PATH.'signin/index/');
+    }
+  }
 }
