d168466750a3a92414369d1398c774f9fef14646
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-06 22:42:05 -0300

    public/application/controllers/AccountController.php: More tidying up.

--- public/application/controllers/AccountController.php
+++ public/application/controllers/AccountController.php
@@ -1,42 +1,21 @@
 <?php
 
-class AccountController extends Zend_Controller_Action
-{
+class AccountController extends Zend_Controller_Action {
 
-    public function init()
-    {
-        /* Initialize action controller here */
-        $this->auth = Zend_Auth::getInstance();
-		
-		if(!$this->auth->hasIdentity())
-			$this->_redirect(PATH.'signin/index/');
+  public function init() {
+    $this->auth = Zend_Auth::getInstance();
+    if (!$this->auth->hasIdentity())
+      $this->_redirect(PATH.'signin/index/');
+  }
 
+  public function changepasswordAction() {
+    if ($this->_getParam("change", "") == "true") {
+      $reg = new Application_Model_Register();
+      $bcrypt = new Application_Model_Bcrypt(20);
+      $password = $bcrypt->hash($this->_getParam("password"));
+      $reg->updateUsersPassword(array('password' => $password), $this->auth->getIdentity()->id);
+      $this->view->success = true;
     }
-
-    public function indexAction()
-    {
-        // action body
-    }
-
-    public function changepasswordAction()
-    {
-        // action body
-        if($this->_getParam("change", "") == "true")
-		{
-			$reg = new Application_Model_Register();
-			
-			$bcrypt = new Application_Model_Bcrypt(20);
-			$password = $bcrypt->hash($this->_getParam("password"));
-			
-			$reg->updateUsersPassword(array('password' => $password), $this->auth->getIdentity()->id);
-			$this->view->success = true;
-		}
-        
-        $this->view->assign('identity', $this->auth->getIdentity());
-    }
-
-
+    $this->view->assign('identity', $this->auth->getIdentity());
+  }
 }
-
-
-
