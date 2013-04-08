8fb04627b7bd035f130981916470dc4e9bdbcec1
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-08 12:57:25 -0300

    public/application/controllers/DashboardController.php: Tidying up.

--- public/application/controllers/DashboardController.php
+++ public/application/controllers/DashboardController.php
@@ -1,41 +1,31 @@
 <?php
 
-class DashboardController extends Zend_Controller_Action
-{
+class DashboardController extends Zend_Controller_Action {
 
-	private $auth;
+  private $auth;
 
-    public function init()
-    {
-        /* Initialize action controller here */
-        $this->auth = Zend_Auth::getInstance();
-		
-		if(!$this->auth->hasIdentity()){
-			$this->_redirect(PATH.'signin/');
-		}
-		
-		$this->view->assign('identity', $this->auth->getIdentity());
+  public function init() {
+    $this->auth = Zend_Auth::getInstance();
+    if (!$this->auth->hasIdentity()) {
+      $this->_redirect(PATH . 'signin/');
+    } else {
+      $this->view->assign('identity', $this->auth->getIdentity());
+    }
+  }
+
+  public function indexAction() {
+    $widgets = new Application_Model_Widgets();
+    $widgets->setIdentity($this->auth->getIdentity()->id);
+    $w = $widgets->getUserWidgets();
+
+    $k = 0;
+    foreach ($w as $key) {
+      if ((time()-(60*60*24)) < strtotime($key['ending'])) $k++;
     }
 
-    public function indexAction()
-    {
-        // action body
-		
-		$widgets = new Application_Model_Widgets();
-		$widgets->setIdentity($this->auth->getIdentity()->id);
-		
-		$w = $widgets->getUserWidgets();
-		
-		$k = 0;
-		foreach ($w as $key)
-			if((time()-(60*60*24)) < strtotime($key['ending']))
-				$k++;
-		$this->view->assign('widgets', $w);
-		$this->view->assign('all', count($w));
-		$this->view->assign('in_progress', $k);
-		$this->view->assign('ended',  count($w) - $k);
-    }
-
-
+    $this->view->assign('widgets', $w);
+    $this->view->assign('all', count($w));
+    $this->view->assign('in_progress', $k);
+    $this->view->assign('ended',  count($w) - $k);
+  }
 }
-
