d06f3025f0c42ddf156f116de4b8895898275646
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-14 14:53:14 -0300

    public/application/forms/Signup.php: Add sanity checking to assert a directory exists in the expected captcha-directory location, and that it is writable by the application.

--- public/application/forms/Signup.php
+++ public/application/forms/Signup.php
@@ -7,9 +7,17 @@ use \MyPHPLibs\URL, \MyPHPLibs\Webapp\CurrentRequest;
 
 class Application_Form_Signup extends Zend_Form {
 
+  private $captchasDir = 'images/captcha';
+
   public function init() {
+    $localPathToCaptchas = DOC_ROOT . '/' . $this->captchasDir;
+    if (!is_dir($localPathToCaptchas)) {
+      throw new Exception("Directory for storing captcha images does not exist");
+    } else if (!is_writable($localPathToCaptchas)) {
+      throw new Exception("Directory for storing captchas is not writable by application");
+    }
     $thisURL = CurrentRequest\getURL();
-    $captchasDirURL = URL\constructUrlFromRelativeLocation($thisURL, '/images/captcha');
+    $captchasDirURL = URL\constructUrlFromRelativeLocation($thisURL, '/' . $this->captchasDir);
     $captcha = new Zend_Form_Element_Captcha('captcha', array(
       'captcha' => array(
         'captcha' => 'Image',
@@ -19,7 +27,7 @@ class Application_Form_Signup extends Zend_Form {
         'height' => '41',
         'font' => 'fonts/arial.ttf',
         'imgUrl' => $captchasDirURL,
-        'imgDir' => 'images/captcha')
+        'imgDir' => $this->captchasDir)
     ));
     $captcha->setDecorators(array(
       array('Description', array('escape' => false, 'tag' => 'div')),
