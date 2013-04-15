1439c1b4c8844fd340a56bd442ada30f86d4849d
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-15 13:22:26 -0300

    public/application/Bootstrap.php: Configure my-php-libs DB connection here (based on parameters provided in Zend config file).

--- public/application/Bootstrap.php
+++ public/application/Bootstrap.php
@@ -1,8 +1,15 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+require_once 'my-php-libs/database.php';
+use \MyPHPLibs\Database as DB;
 
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
+  public function _initDatabaseConn() {
+    $options = $this->getOptions();
+    $params = $options['resources']['db']['params'];
+    DB\setConnectionParams($driver = 'mysql', $dbName = $params['dbname'],
+      $username = $params['username'], $password = $params['password'],
+      $host = $params['host']);
+  }
 }
-
