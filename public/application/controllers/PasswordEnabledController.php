3b90e280a1327b76bc0394ef42efc10acee04713
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-19 16:25:53 -0300

    public/application/controllers/PasswordEnabledController.php: Prefix password hash with 'bcrypt' (in method 'passwordHash') to provide for more flexibility in hashing mechanism used in the future; make 'passwordVerify' take this prefix into account.

--- public/application/controllers/PasswordEnabledController.php
+++ public/application/controllers/PasswordEnabledController.php
@@ -1,18 +1,16 @@
 <?php
 
-/**
- * F*** Zend Framework -- I can't even have common base-classes for controllers?
- */
-
 require_once 'my-php-libs/password-hashing.php';
+require_once 'my-php-libs/string.php';
 
 abstract class PasswordEnabledController extends Zend_Controller_Action {
 
   protected function passwordHash($password) {
-    return password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
+    return 'bcrypt' . password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
   }
 
   protected function passwordVerify($password, $hash) {
-    return password_verify($password, $hash);
+    $unprefixedHash = withoutPrefix($hash, 'bcrypt');
+    return password_verify($password, $unprefixedHash);
   }
 }
