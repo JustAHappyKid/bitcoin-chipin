2ba1df655313ee9d45c56c1743135888462bd9fc
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-25 21:46:15 -0300

    lib/chipin/: Add bitcoin.php lib, which at present contains functions 'getBalance' (to retrieve the balance for a particular Bitcoin address) and 'toBTC' (for converting a fiat-currency value to BTC).

---
+++ lib/chipin/bitcoin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Chipin\Bitcoin;
+
+require_once 'my-php-libs/web-client/http-simple.php';
+use \MyPHPLibs\WebClient\HttpSimple as Http;
+
+function getBalance($address, $currency = 'BTC') {
+  $btcBalance = Http\get('http://blockchain.info/q/addressbalance/' . $address) / 100000000;
+  /*
+  $balanceWithPrecision = Http\get('http://blockchain.info/tobtc' .
+                                   '?currency=' . $currency . '&value=' . $value);
+  */
+  $balanceWithPrecision = $currency == 'BTC' ? $btcBalance : toBTC($currency, $btcBalance);
+  $balance = substr($balanceWithPrecision, 0, 4);
+  return $balance;
+}
+
+function toBTC($currency, $amount) {
+  return (float) Http\get('http://blockchain.info/tobtc?currency=USD&value=1');
+}
