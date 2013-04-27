7262fbc54d7fb6a8ca23aa12a7206f0098cb562f
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-27 20:18:19 -0300

    public/application/models/Users.php: Insert password-reset record into DB before sending email; some other code tidying up.

--- public/application/models/Users.php
+++ public/application/models/Users.php
@@ -15,14 +15,6 @@ class Application_Model_Users {
     } else {
       $result = $this->generateLink(8, $user[0]['id']);
 
-      $mail = new Zend_Mail();
-      $mail->setFrom('webmaster@bitcoinchipin.com');
-      $mail->addTo($email, 'recipient');
-      $mail->setSubject('BitcoinChipin.com Password Reset');
-      $mail->setBodyText(
-        "Please use the following link to reset your password:\n\n" . $result['url']);
-      $mail->send();
-
       $this->_dbTable->insert('reset_password', array(
         'user_id' => $user[0]['id'],
         'link' => $result['url'],
@@ -30,6 +22,14 @@ class Application_Model_Users {
         'code' => $result['code'],
         'status' => 1
       ));
+
+      $mail = new Zend_Mail();
+      $mail->setFrom('webmaster@bitcoinchipin.com');
+      $mail->addTo($email, 'recipient');
+      $mail->setSubject('BitcoinChipin.com Password Reset');
+      $mail->setBodyText(
+        "Please use the following link to reset your password:\n\n" . $result['url']);
+      $mail->send();
       return true;
     }
   }
@@ -39,26 +39,19 @@ class Application_Model_Users {
     $possible = "2346789bcdfghjkmnpqrtvwxyzBCDFGHJKLMNPQRTVWXYZ";
 
     $maxlength = strlen($possible);
-    if ($length > $maxlength)
-    {
-      $length = $maxlength;
-    }
+    if ($length > $maxlength) $length = $maxlength;
+
     $i = 0;
-
-    while ($i < $length)
-    {
-
+    while ($i < $length) {
       $char = substr($possible, mt_rand(0, $maxlength-1), 1);
-
-      if (!strstr($password, $char))
-      {
+      if (!strstr($password, $char)) {
         $password .= $char;
         $i++;
       }
     }
-      return array(
-        'url' => PATH . 'signin/approve/?code=' . $password . '&user_id=' . $user_id,
-        'code' => $password
+    return array(
+      'url' => PATH . 'signin/approve/?code=' . $password . '&user_id=' . $user_id,
+      'code' => $password
     );
   }
 
