c9e36325006b525dc19349e8c5d806d4f4993810
Chris Wagner <christopher.t.wagner@gmail.com>
2013-05-02 16:26:15 -0400

    public/application/controllers/AdminController.php: Just some tidying up.

--- public/application/controllers/AdminController.php
+++ public/application/controllers/AdminController.php
@@ -1,69 +1,41 @@
 <?php
 
-class AdminController extends Zend_Controller_Action
-{
+class AdminController extends Zend_Controller_Action {
 
-    private $auth = null;
+  private $auth = null;
 
-    public function init()
-    {
-        /* Initialize action controller here */
-        $this->auth = Zend_Auth::getInstance();
-		
-		if(!$this->auth->hasIdentity()){
-			$this->_redirect(PATH.'signin/index/');
-		}
-		
-		$this->view->assign('identity', $this->auth->getIdentity());
-		
-		$this->_helper->layout->setLayout('admin');
+  public function init() {
+    $this->auth = Zend_Auth::getInstance();
+    if (!$this->auth->hasIdentity()){
+      $this->_redirect(PATH.'signin/index/');
     }
+    $this->view->assign('identity', $this->auth->getIdentity());
+    $this->_helper->layout->setLayout('admin');
+  }
 
-    public function indexAction()
-    {
-        // action body
-		$statistcis = new Application_Model_Statistics();
-		
-		$this->view->assign('widgets', $statistcis->getRecentWidgets());
-		$this->view->assign('total_widgets_created', $statistcis->getTotalNumberOfWidgetsCreated());
-		$this->view->assign('new_widgets', $statistcis->getNewWidgetsStatistics());
-		$this->view->assign('ending_widgets', $statistcis->getEndingWidgetsStatistics());
-		$this->view->assign('top_countries', $statistcis->getTopCountries());
-		$this->view->assign('fastest_growing_widgets', $statistcis->getFastestGrowingWidgets());
-		$this->view->assign('top_active_users', $statistcis->getTopActiveUsers());
-    }
+  public function indexAction() {
+    $statistcis = new Application_Model_Statistics();
+    $this->view->assign('widgets', $statistcis->getRecentWidgets());
+    $this->view->assign('total_widgets_created', $statistcis->getTotalNumberOfWidgetsCreated());
+    $this->view->assign('new_widgets', $statistcis->getNewWidgetsStatistics());
+    $this->view->assign('ending_widgets', $statistcis->getEndingWidgetsStatistics());
+    $this->view->assign('top_countries', $statistcis->getTopCountries());
+    $this->view->assign('fastest_growing_widgets', $statistcis->getFastestGrowingWidgets());
+    $this->view->assign('top_active_users', $statistcis->getTopActiveUsers());
+  }
 
-    public function listingAction()
-    {
-        // action body
-        $statistcis = new Application_Model_Statistics();
-		
-		$this->view->assign('widgets', $statistcis->getAllWidgets());
-    }
+  public function listingAction() {
+    $statistcis = new Application_Model_Statistics();
+    $this->view->assign('widgets', $statistcis->getAllWidgets());
+  }
 
-    public function widgetsAction()
-    {
-        // action body
-    }
+  public function widgetsAction() {}
 
-    public function endAction()
-    {
-        // action body
-        $id = $this->_getParam('id', '');
-		$widget = new Application_Model_Widgets();
-		$widget->setIdentity($this->auth->getIdentity()->id);
-		
-		$widget->endWidget($id);
-		
-		$this->_redirect(PATH.'admin/');
-    }
-
-
+  public function endAction() {
+    $id = $this->_getParam('id', '');
+    $widget = new Application_Model_Widgets();
+    $widget->setIdentity($this->auth->getIdentity()->id);
+    $widget->endWidget($id);
+    $this->_redirect(PATH.'admin/');
+  }
 }
-
-
-
-
-
-
-
