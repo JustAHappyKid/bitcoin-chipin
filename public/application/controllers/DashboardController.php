9dec551cedda704c05a18c07705dceef103ca4bc
Chris Wagner <christopher.t.wagner@gmail.com>
2013-05-03 00:19:13 -0400

    public/application/controllers/WidgetsController.php: Add hook to assure progress / amount-raised are updated (at least every ten minutes) when viewing dashboard; and, query for widgets using \Chipin\Widgets functionality.

--- public/application/controllers/DashboardController.php
+++ public/application/controllers/DashboardController.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once 'chipin/widgets.php';
+use \Chipin\Widgets;
+
 class DashboardController extends Zend_Controller_Action {
 
   private $auth;
@@ -14,18 +17,31 @@ class DashboardController extends Zend_Controller_Action {
   }
 
   public function indexAction() {
+    /*
     $widgets = new Application_Model_Widgets();
     $widgets->setIdentity($this->auth->getIdentity()->id);
     $w = $widgets->getUserWidgets();
+    */
+
+    $user = $this->auth->getIdentity();
+    $widgets = Widgets\getByOwner($user);
+
+    $tenMinutes = 60 * 10;
+    $lastProgressUpdate = @ $_SESSION['lastProgressUpdate'];
+    if (empty($lastProgressUpdate) || $lastProgressUpdate + $tenMinutes < time()) {
+      foreach ($widgets as $w) Widgets\updateProgress($w);
+      $widgets = Widgets\getByOwner($user);
+      $_SESSION['lastProgressUpdate'] = time();
+    }
 
     $k = 0;
-    foreach ($w as $key) {
+    foreach ($widgets as $key) {
       if ((time()-(60*60*24)) < strtotime($key['ending'])) $k++;
     }
 
-    $this->view->assign('widgets', $w);
-    $this->view->assign('all', count($w));
+    $this->view->assign('widgets', $widgets);
+    $this->view->assign('all', count($widgets));
     $this->view->assign('in_progress', $k);
-    $this->view->assign('ended',  count($w) - $k);
+    $this->view->assign('ended',  count($widgets) - $k);
   }
 }
