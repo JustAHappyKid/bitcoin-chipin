b655c3c4ef4464e51c234745db564e970b4421bc
Chris Wagner <christopher.t.wagner@gmail.com>
2013-05-02 23:36:31 -0400

    lib/chipin/debug.php: Add functions 'info' and 'error'.

--- lib/chipin/debug.php
+++ lib/chipin/debug.php
@@ -4,4 +4,6 @@ require_once 'my-php-libs/types.php'; # for access to 'asString'
 
 require_once 'my-php-libs/log.php';
 configureLogging('/var/local/log/chipin-dev/webapp.log');
-function debug($m) { return logMsg('debug', $m); }
+function debug($m)  { return logMsg('debug', $m); }
+function info($m)   { return logMsg('info', $m); }
+function error($m)  { return logMsg('error', $m); }
