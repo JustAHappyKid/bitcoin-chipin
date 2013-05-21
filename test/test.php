5aa5c2752caa3c252ed64aae23e593a8c71590b9
Chris Wagner <christopher.t.wagner@gmail.com>
2013-05-20 21:51:12 -0400

    test/test.php: Add Zend Framework to path (albeit temporarily) so those libs are available to tested code as well.

--- test/test.php
+++ test/test.php
@@ -7,6 +7,11 @@ function main($argc, $argv) {
   $testDir = realpath(dirname(__FILE__));
   $libsDir = realpath(dirname($testDir) . '/lib');
   set_include_path($libsDir . PATH_SEPARATOR . get_include_path());
+  
+  # XXX: Zend Framework required for now, unfortunately (for Captcha lib, at least)...
+  set_include_path('/usr/local/lib/php5/ZendFramework-1/library/' .
+    PATH_SEPARATOR . get_include_path());
+
   require_once 'spare-parts/database.php';
   DB\setConnectionParams($driver = 'mysql', $dbName = 'chipin_test',
     $username = 'chipin_test', $password = 'password', $host = 'localhost');
