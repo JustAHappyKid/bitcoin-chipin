ab8199a764addc168b8c0cae3616759aa4ef800f
Chris Wagner <christopher.t.wagner@gmail.com>
2013-05-23 15:07:58 -0400

    Implement new theme (Measure) for base layout and signup form.

--- templates/account/signup.php
+++ templates/account/signup.php
@@ -8,124 +8,107 @@ class Signup extends Layout {
 
   function innerContent() { ?>
 
-    <link href="/css/components/signin.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="/measure-theme/css/signup.css" />
+    <link href="/css/components/signup.css" rel="stylesheet" type="text/css" />
+    <!-- <link href="/css/components/signin.css" rel="stylesheet" type="text/css" /> -->
+
     <?= $this->validationJavascript(); ?>
 
-    <style type="text/css">
-      #captcha-input{
-        width: 170px;
-        float: left;
-        margin-left: 30px;
-      }
-
-      .field img{
-        float: left;
-        height: 40px;
-      }
-
-      body {
-        background: white url("<?=PATH;?>img/bkg_body.gif") 50% 0 repeat-x;
-      }
-
-      .page {
-        background: url("<?=PATH;?>img/bkg_top_bar.png") no-repeat scroll 0 0 white;
-        box-shadow: 0 1px 5px #AAA;
-        margin: 20px auto;
-        padding: 17px;
-        width: 979px;
-        min-height: 600px;
-      }
-    </style>
-
-    <div class="page">
-      <div class="account-container register">
-        <div class="bitcoinchipin-logo">
-          <img src="<?=PATH?>img/logo.jpg" alt="Bitcoin Chipin" />
-        </div>
-        <div class="content clearfix">
-          <form action="<?=PATH;?>account/signup" onsubmit="return validateForm();" method="post">
-            <h1>Create Your Account</h1>			
-            <? /* <div class="login-social" style="display: none;">
-              <p>Sign in using social network:</p>
-              <div class="twitter">
-                <a href="#" class="btn_1">Login with Twitter</a>				
+    <div id="box_sign">
+      <div class="container">
+        <div class="span12 box_wrapper">
+          <div class="span12 box">
+            <div class="head">
+              <div class="bitcoinchipin-logo">
+                <img src="<?=PATH?>img/logo.jpg" alt="Bitcoin Chipin" />
               </div>
-              <div class="fb">
-                <a href="#" class="btn_2">Login with Facebook</a>				
-              </div>
-            </div> */ ?>
-            <? if ($this->form->hasErrors()): ?>
-              <div id="form-error" class="alert alert-error">
-                <a class="close" data-dismiss="alert" href="#">×</a>
-                <h4 class="alert-heading">Error!</h4>
-                <?= current($this->form->getErrors()) ?>
-              </div>
-            <? endif; ?>
-            <div id="password-error" class="alert alert-error" style="display: none;">
-              <a class="close" data-dismiss="alert" href="#">×</a>
-              <h4 class="alert-heading">Error!</h4>
-              Passwords don't match.
+              <h4>Let's get you an account</h4>
             </div>
-            <div id="username-error" class="alert alert-error" style="display: none;">
-              <a class="close" data-dismiss="alert" href="#">×</a>
-              <h4 class="alert-heading">Error!</h4>
-              You must provide a username!
-            </div>
-            <?php if (isset($this->captchaIncorrect) && $this->captchaIncorrect): ?>
-              <div class="alert alert-error">
-                <a class="close" data-dismiss="alert" href="#">×</a>
-                <h4 class="alert-heading">Error!</h4>
-                Captcha code incorrectly identified. Please try again!
-              </div>
-            <?php endif; ?>
-            <div class="login-fields">
-              <p>Create your free account:</p>
-              <div class="field">
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" value="" placeholder="Username" class="login" />
-              </div> <!-- /field -->
-              <div class="field">
-                <label for="email">Email Address:</label>
-                <input type="text" id="email" name="email" value="" placeholder="Email" class="login"/>
-              </div> <!-- /field -->
-              <div class="field">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password1" value=""
-                       placeholder="Password" class="login"/>
-              </div> <!-- /field -->
-              <div class="field">
-                <label for="confirm_password">Confirm Password:</label>
-                <input type="password" id="confirm_password" name="password2" value=""
-                       placeholder="Confirm Password" class="login"/>
-              </div> <!-- /field -->
-              <div class="field">
-                <?= $this->captcha->render() ?>
-                <input type="hidden" name="captcha-id" id="captcha-id"
-                       value="<?= $this->captcha->getId() ?>" />
-                <input type="text" name="captcha-input" id="captcha-input" value=""
-                       placeholder="Captcha">
-                <div style="clear: both;"> </div>
-              </div>
-            </div> <!-- /login-fields -->
-            <div class="login-actions">
-              <? /* <span class="login-checkbox" style="display: none;">
-                <input id="Field" name="Field" type="checkbox" class="field login-checkbox"
-                       value="First Choice" tabindex="4" />
-                <label class="choice" for="Field">I have read and agree with the Terms of Use.</label>
-              </span> */ ?>
-              <button class="button btn btn-primary btn-large">Register</button>
-            </div> <!-- .login-actions -->
-          </form>
-        </div> <!-- /content -->
-        
-      </div> <!-- /account-container -->
+            <div class="form">
+              <form action="<?=PATH;?>account/signup" method="post"
+                    onsubmit="return validateForm();">
+                <? /* <div class="login-social" style="display: none;">
+                  <p>Sign in using social network:</p>
+                  <div class="twitter">
+                    <a href="#" class="btn_1">Login with Twitter</a>
+                  </div>
+                  <div class="fb">
+                    <a href="#" class="btn_2">Login with Facebook</a>
+                  </div>
+                </div> */ ?>
+                <? if ($this->form->hasErrors()): ?>
+                  <div id="form-error" class="alert alert-error">
+                    <a class="close" data-dismiss="alert" href="#">×</a>
+                    <h4 class="alert-heading">Error!</h4>
+                    <?= current($this->form->getErrors()) ?>
+                  </div>
+                <? endif; ?>
+                <div id="password-error" class="alert alert-error" style="display: none;">
+                  <a class="close" data-dismiss="alert" href="#">×</a>
+                  <h4 class="alert-heading">Error!</h4>
+                  Passwords don't match.
+                </div>
+                <div id="username-error" class="alert alert-error" style="display: none;">
+                  <a class="close" data-dismiss="alert" href="#">×</a>
+                  <h4 class="alert-heading">Error!</h4>
+                  You must provide a username!
+                </div>
+                <?php if (isset($this->captchaIncorrect) && $this->captchaIncorrect): ?>
+                  <div class="alert alert-error">
+                    <a class="close" data-dismiss="alert" href="#">×</a>
+                    <h4 class="alert-heading">Error!</h4>
+                    Captcha code incorrectly identified. Please try again!
+                  </div>
+                <?php endif; ?>
+                <div class="login-fields">
+                  <div class="field">
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" value=""
+                          placeholder="Username" class="login" />
+                  </div> <!-- /field -->
+                  <div class="field">
+                    <label for="email">Email Address:</label>
+                    <input type="text" id="email" name="email" value="" placeholder="Email" class="login"/>
+                  </div> <!-- /field -->
+                  <div class="field">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password1" value=""
+                          placeholder="Password" class="login"/>
+                  </div> <!-- /field -->
+                  <div class="field">
+                    <label for="confirm_password">Confirm Password:</label>
+                    <input type="password" id="confirm_password" name="password2" value=""
+                          placeholder="Confirm Password" class="login"/>
+                  </div> <!-- /field -->
+                  <div class="field captcha-field">
+                    <?= $this->captcha->render() ?>
+                    <input type="hidden" name="captcha-id" id="captcha-id"
+                          value="<?= $this->captcha->getId() ?>" />
+                    <input type="text" name="captcha-input" id="captcha-input" value=""
+                          placeholder="Captcha">
+                    <div style="clear: both;"> </div>
+                  </div>
+                </div> <!-- /login-fields -->
+                <div class="login-actions">
+                  <? /* <span class="login-checkbox" style="display: none;">
+                    <input id="Field" name="Field" type="checkbox" class="field login-checkbox"
+                          value="First Choice" tabindex="4" />
+                    <label class="choice" for="Field">I have read and agree with the Terms of Use.</label>
+                  </span> */ ?>
+                  <!-- <button class="button btn btn-primary btn-large">Register</button> -->
+                  <input type="submit" class="btn btn-large" value="Register" />
+                </div> <!-- /.login-actions -->
+              </form>
+            </div> <!-- /.form -->
+          </div> <!-- /.box -->
 
-      <!-- Text Under Box -->
-      <div class="login-extra">
-        Already have an account? <a href="<?=PATH;?>signin/index/">Sign in here.</a>
-      </div> <!-- /login-extra -->
+          <p class="already login-extra">
+            Already have an account? <a href="<?=PATH;?>signin/index/">Sign in here.</a>
+          </p>
 
-    </div>
+        </div> <!-- /.box_wrapper -->
+      </div> <!-- /.container -->
+    </div> <!-- /#box_sign -->
 
   <? }
 
@@ -139,7 +122,7 @@ class Signup extends Layout {
           if ($("#username-error").size() != 0) {
             $("#username-error").hide();
           }
-        }        
+        }
         if ($("#password").val() == '' || $("#password").val() != $("#confirm_password").val()) {
           $("#password-error").show();
           return false;
--- templates/layout.php
+++ templates/layout.php
@@ -9,58 +9,96 @@ abstract class Layout {
     ?><!DOCTYPE html>
     <html lang="en">
       <head>
-        <? require dirname(dirname(__FILE__)) . '/public/application/layouts/scripts/head-common.phtml'; ?>
 
-        <style type="text/css">
+        <? /* TODO: Need to include Analytics snippet and what else?? */ ?>
 
-          #nav .container, #footer .container {
-	          width: 1013px;
-          }
+        <? /* require dirname(dirname(__FILE__)) . '/public/application/layouts/scripts/head-common.phtml'; */ ?>
 
-          #content .container {
-	          background: url("<?= PATH ?>img/bkg_top_bar.png") no-repeat scroll 0 0 white;
-	          box-shadow: 0 1px 5px #AAA;
-	          width: 979px;
-	          padding: 17px;
-	          margin-left: auto;
-	          margin-right: auto;
-	          min-height: 500px;
-          }
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>BitcoinChipin.com</title>
 
-        </style>
+        <? $themeDir = '/measure-theme'; ?>
+        <link rel="stylesheet" type="text/css" href="/measure-theme/css/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" href="/measure-theme/css/theme.css" />
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic'
+              rel='stylesheet' type='text/css' />
+
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="<?= $themeDir ?>/js/bootstrap.min.js"></script>
+        <script src="<?= $themeDir ?>/js/theme.js"></script>
+
       </head>
 
       <body>
 
         <? if ($this->userIsAuthenticated()): ?>
           <div id="nav">
-	          <div class="container">
-		          <div class="nav-collapse">
-			          <ul class="nav">
-				          <li class="nav-icon">
-					          <a href="<?=PATH;?>">
-						          <i class="icon-home"></i>
-						          <span>Home</span>
-					          </a>	    				
-				          </li>
-			          </ul>
-			          <ul class="nav pull-right">
-				          <li class="dropdown">					
-					          <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
-						          <i class="icon-external-link"></i>
-						          Account
-						          <b class="caret"></b>
-					          </a>
-					          <ul class="dropdown-menu">
-						          <li><a href="<?=PATH.'account/changepassword/';?>">Change password</a></li>
-						          <li><a href="<?=PATH.'signin/signout/';?>">Sign out</a></li>
-					          </ul>    				
-				          </li>
-			          </ul>
-		          </div> <!-- /.nav-collapse -->
-	          </div> <!-- /.container -->
+            <div class="container">
+              <div class="nav-collapse">
+                <ul class="nav">
+                  <li class="nav-icon">
+                    <a href="<?=PATH;?>">
+                      <i class="icon-home"></i>
+                      <span>Home</span>
+                    </a>
+                  </li>
+                </ul>
+                <ul class="nav pull-right">
+                  <li class="dropdown">
+                    <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
+                      <i class="icon-external-link"></i>
+                      Account
+                      <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a href="<?=PATH.'account/changepassword/';?>">Change password</a></li>
+                      <li><a href="<?=PATH.'signin/signout/';?>">Sign out</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div> <!-- /.nav-collapse -->
+            </div> <!-- /.container -->
           </div> <!-- /#nav -->
         <? endif; ?>
+
+        <div class="navbar navbar-fixed-top">
+          <div class="navbar-inner">
+            <div class="container">
+              <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </a>
+              <a class="brand" href="index.html" style="min-height: 35px;">
+                <!-- <img src="img/logo.png" alt="logo" /> -->
+              </a>
+              <div class="nav-collapse collapse">
+                <ul class="nav pull-right">
+                  <li><a href="/about/">About us</a></li>
+                  <li><a href="/about/contact">Contact us</a></li>
+                  <!-- <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                          External Pages
+                          <b class="caret"></b>
+                      </a>
+                      <ul class="dropdown-menu">
+                          <li><a href="features.html">Features</a></li>
+                          <li><a href="pricing.html">Pricing</a></li>
+                          <li><a href="portfolio.html">Portfolio</a></li>
+                          <li><a href="coming-soon.html">Coming Soon</a></li>
+                          <li><a href="aboutus.html">About us</a></li>
+                          <li><a href="contact.html">Contact us</a></li>
+                          <li><a href="faq.html">FAQ</a></li>
+                      </ul>
+                  </li> -->
+                  <li><a class="btn-header" href="/account/signup">Sign up</a></li>
+                  <li><a class="btn-header" href="/signin/index/">Sign in</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <?= $this->innerContent(); ?>
 
