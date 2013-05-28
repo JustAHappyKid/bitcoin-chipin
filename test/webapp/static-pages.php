30438ce43473fef6d5071321e7e24c5fc58dfa47
Chris Wagner <christopher.t.wagner@gmail.com>
2013-05-27 23:31:48 -0400

    test/webapp/: Add static-pages.php, which as of now just "touches" the homepage.

---
+++ test/webapp/static-pages.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Chipin\Test;
+
+/**
+ * In this test-class/file we're just "touching" the (primarily) static pages, such
+ * as the homepage, to make sure they at least render without anything blowing up.
+ */
+class StaticPagesTests extends WebappTestingHarness {
+
+  function testHomepage() {
+    $this->get('/');
+  }
+}
