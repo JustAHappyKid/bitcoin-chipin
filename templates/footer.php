ad0664189a0262d19e4b86d965f06256e2b681e6
Chris Wagner <christopher.t.wagner@gmail.com>
2013-05-28 19:29:11 -0400

    templates/: Break site-footer into separate file, to allow code-reuse with components still under Zend Framework's domain.

---
+++ templates/footer.php
@@ -0,0 +1,78 @@
+
+        <div id="footer">
+          <div class="container">
+            <div class="row">
+              <div class="span8">
+                <h3>Bitcoin resources</h3>
+                  <div class="blog_post">
+                    <div class="row">
+                      <div class="span2">
+                        <a href="bitcoins.html">
+                          <img src="img/bitcoin.png" alt="post image" class="img-circle" />
+                        </a>
+                      </div>
+                      <div class="span4">
+                        <a href="http://www.weusecoins.com/" class="title">WeUseCoins</a>
+                        <div class="intro">
+                          "Our mission is to support Bitcoin by making it easier for
+                          new users to get started."
+                        </div>
+                      </div>
+        							<div class="span4">
+                        <a href="http://bitcoin.cbtnuggets.com/" class="title">
+                          Bitcoin Training Series from CBT Nuggets
+                        </a>
+                        <div class="intro">
+                          "Learn what it is, how it works, and how you can get started
+                          using it in this groundbreaking CBT Nuggets series with trainer
+                          Keith Barker"
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="span4 contact_us">
+                  <h3>Contact Us</h3>
+                  <form class="form-horizontal">
+                    <div class="field">
+                      <div class="label_field">
+                        <label for="inputName">Name</label>
+                      </div>
+                      <input type="text" id="inputName" />
+                    </div>
+                    <div class="field">
+                      <div class="label_field">
+                        <label for="inputEmail">Email</label>
+                      </div>
+                      <input type="text" id="inputEmail" />
+                    </div>
+                    <div class="field">
+                      <div class="label_field">
+                        <label for="inputCity">City</label>
+                      </div>
+                      <input type="text" id="inputCity" />
+                    </div>
+                    <div class="field">
+                      <div class="label_field">
+                        <label for="inputComment">Comments</label>
+                      </div>
+                      <textarea name="comments"></textarea>
+                    </div>
+                    <button type="submit" class="btn">Send</button>
+                  </form>
+                </div>
+              </div> <!-- /.contact_us -->
+            <hr/>
+            <div class="row copyright">
+              <div class="span5">
+                <h3>Memorydealers</h3>
+                <a href="#" class="social fb"><i class="i_facebook"></i></a>
+                <a href="#" class="social tw"><i class="i_twitter"></i></a>
+                <a href="#" class="social yt"><i class="i_youtube"></i></a>
+              </div>
+              <div class="span2 offset5 copy">
+                <p>&#169; 2013 - Bitcoinchipin.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
