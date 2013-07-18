187c07ee5de780b15e6e9d4e66b3843d838c3611
Chris Wagner <christopher.t.wagner@gmail.com>
2013-07-18 19:50:03 -0400

    lib/chipin/currency.php: Display fiat amounts over 100 (aside JPY) without cents/pence/etc.

--- lib/chipin/currency.php
+++ lib/chipin/currency.php
@@ -9,6 +9,10 @@ function displayAmount($amount, $currency) {
     $maxDecimalPlaces = strlen((string) ceil($millibits));
   } else if ($currency == 'JPY') {
     $maxDecimalPlaces = 0;
+  } else if ($amount > 100) {
+    # If it's a fiat currency (other than JPY), don't bother displaying cents/pence
+    # when amount is over 100 dollars/pounds/euros/etc.
+    $maxDecimalPlaces = 0;
   }
   return number_format($amount, $maxDecimalPlaces) . ' ' . $currency;
 }
