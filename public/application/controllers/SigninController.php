d61c8d0c49d81f380b1513efc9a17f8aa1578dc3
Chris Wagner <christopher.t.wagner@gmail.com>
2013-07-23 13:11:29 -0400

    public/application/controllers/SigninController.php: Remove signin-related actions (as signin is now performed via custom, spare-parts-based framework) and now-unused method.

--- public/application/controllers/SigninController.php
+++ public/application/controllers/SigninController.php
@@ -11,86 +11,6 @@ class SigninController extends PasswordEnabledController {
     $this->_helper->layout->setLayout('no-auth-layout');
   }
 
-  public function indexAction() {
-    $cp = $this->_getParam('cp', 0);
-    if (isset($cp) && $cp) {
-      $code = $this->_getParam('c');
-      // $userID = $this->_getParam('u');
-      if ($this->isValidConfirmationCode($code)) {
-        $user = ConfCodes\getUserForCode($code);
-        $hashedPass = $this->passwordHash($this->_getParam("password"));
-        $user = User::loadFromID($user->id);
-        $user->updatePassword($hashedPass);
-        $this->removeConfirmationCode($code);
-        $this->view->passwordReset = true;
-      } else {
-        $this->_redirect(PATH . 'signin/expired/c/'.$code.'/u/' . $userID);
-      }
-    }
-
-    $form = new Application_Form_Signin();
-    $this->view->success = $this->_getParam("s");
-    $this->view->form = $form;
-
-    if ($this->_getParam('signin', '') == 'failure') {
-      $this->view->assign('failure', true);
-    }
-    if ($this->_getParam('remind', '') == 'success') {
-      $this->view->assign('success', true);
-    }
-  }
-
-  public function authAction() {
-    $request = $this->getRequest();
-    if ($request->isPost()) {
-      $username = $this->_getParam('username');
-      $password = $this->_getParam('password');
-      if (!empty($username) && !empty($password)) {
-        $existingHash = $this->getStoredHashForUser($username);
-        $isGood = $this->passwordVerify($password, $existingHash);
-        if ($isGood) {
-          $dbAdapter = Zend_Db_Table::getDefaultAdapter();
-          $authAdapter = new Zend_Auth_Adapter_DbTable($dbAdapter);
-          
-          $authAdapter->setTableName("users");
-          $authAdapter->setIdentityColumn("username");
-          $authAdapter->setCredentialColumn("password");
-          
-          $authAdapter->setIdentity($username);
-          $authAdapter->setCredential($this->passwordHash($password));
-          
-          $auth = Zend_Auth::getInstance();
-          $result = $auth->authenticate($authAdapter);
-          
-          // XXX !!!
-          //if ($result->isValid()) {
-          if ($isGood) {
-            // XXX !!!
-            // $identity = $authAdapter->getResultRowObject();
-            $identity = User::loadFromUsername($username);
-            
-            $authStorage = $auth->getStorage();
-            $authStorage->write($identity);
-
-            /*
-            if($identity->role == 'admin') {
-              $this->_redirect(PATH.'admin/');
-            }
-            */
-
-            $this->_redirect(PATH.'dashboard/');
-          } else {
-            $this->_redirect(PATH.'signin/index/signin/failure/');
-          }
-        } else {
-          $this->_redirect(PATH.'signin/index/signin/failure/');
-        }
-      } else {
-        $this->_redirect(PATH.'signin/index/');
-      }
-    }
-  }
-
   public function signoutAction() {
     $auth = Zend_Auth::getInstance();
     $auth->clearIdentity();
@@ -141,14 +61,5 @@ class SigninController extends PasswordEnabledController {
 
   private function removeConfirmationCode($code, $_ = null) {
     ConfCodes\removeCode($code);
-  }
-
-  private function getStoredHashForUser($username) {
-    $_dbTable = Zend_Db_Table::getDefaultAdapter();
-    $select = $_dbTable->select()
-      ->from('users', 'password')
-      ->where('username = "'.$username.'"');
-    $result = $_dbTable->fetchAll($select);
-    return $result[0]['password'];
   }
 }
