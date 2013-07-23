d83031faf09d9c9665c64b2aad55403a29e3518b
Chris Wagner <christopher.t.wagner@gmail.com>
2013-07-23 18:02:36 -0400

    test/webapp/admin/basics.php: Add simple test for /admin/users/ screen.

--- test/webapp/admin/basics.php
+++ test/webapp/admin/basics.php
@@ -2,9 +2,10 @@
 
 namespace Chipin\Test;
 
-use SpareParts\Test\HttpNotFound;
-
 require_once dirname(dirname(__FILE__)) . '/harness.php';
+require_once 'spare-parts/database.php';
+
+use \SpareParts\Test\HttpNotFound, \SpareParts\Database as DB;
 
 class AdminBasicTests extends WebappTestingHarness {
 
@@ -13,5 +14,14 @@ class AdminBasicTests extends WebappTestingHarness {
     try {
       $this->get('/admin/users/');
     } catch (HttpNotFound $_) { /* That's good. */ }
+  }
+
+  function testUsersOverviewScreen() {
+    $u1 = newUser('some-user@example.com', 'some-user', 'abc');
+    $u2 = newUser('other-user@example.com', 'other-user', 'def');
+    DB\updateByID('users', $u2->id, array('created_at' => new \DateTime('now')));
+    newUser('chris@easyweaze.net', 'chris', 'secret!');
+    $this->login('chris', 'secret!');
+    $this->get('/admin/users/');
   }
 }
