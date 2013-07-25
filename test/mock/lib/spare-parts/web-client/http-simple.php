e80565838e249095fcab302e15ff7417c77a3106
Chris Wagner <christopher.t.wagner@gmail.com>
2013-07-24 21:47:54 -0400

    test/mock/lib/spare-parts/web-client/http-simple.php: Properly import Exception into namespace; broaden URLs matched-on for 'http://blockchain.info/tobtc'.

--- test/mock/lib/spare-parts/web-client/http-simple.php
+++ test/mock/lib/spare-parts/web-client/http-simple.php
@@ -2,10 +2,12 @@
 
 namespace SpareParts\WebClient\HttpSimple;
 
+use \Exception;
+
 function get($url) {
   if ($url == 'http://blockchain.info/q/addressbalance/1PUPt26votHesaGwSApYtGVTfpzvs8AxVM') {
     return '2537813';
-  } else if ($url == 'http://blockchain.info/tobtc?currency=USD&value=1') {
+  } else if (beginsWith($url, 'http://blockchain.info/tobtc?currency=USD')) {
     return '0.00778083';
   } else if ($url == 'http://api.easyjquery.com/ips/?ip=99.99.99.99') {
     return '{"Country": "CH"}';
