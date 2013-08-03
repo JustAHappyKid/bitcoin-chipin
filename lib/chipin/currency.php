93dd908a9d1db9f5f6490127c09b9066fafa47a1
Chris Wagner <christopher.t.wagner@gmail.com>
2013-08-03 14:43:07 -0400

    lib/chipin/currency.php: Add 'Amount' class, to give us some type safety when passing around amounts of money (e.g., 200 USD or 2.75 BTC); and add function 'trimZeros' for removing excess zeros from the beginning and end of a numeric string.

--- lib/chipin/currency.php
+++ lib/chipin/currency.php
@@ -2,6 +2,33 @@
 
 namespace Chipin\Currency;
 
+use \InvalidArgumentException, \Chipin\Currencies;
+
+# XXX: Rename to SumOfMoney?
+class Amount {
+
+  /** @var Currency */
+  //public $currency;
+
+  public $currencyCode;
+
+  /** @var int */
+  public $numUnits;
+
+  function __construct($c, $u) {
+    if (!in_array($c, Currencies\codes()))
+      throw new InvalidArgumentException("'$c' is not a recognized currency-code");
+    if (!is_numeric($u))
+      throw new InvalidArgumentException("Second parameter must be valid numeric value");
+    $this->currencyCode = $c;
+    $this->numUnits = doubleval($u);
+  }
+
+  function __toString() {
+    return displayAmount($this->numUnits, $this->currencyCode);
+  }
+}
+
 function displayAmount($amount, $currency) {
   $maxDecimalPlaces = 2;
   if ($currency == 'BTC') {
@@ -15,4 +42,14 @@ function displayAmount($amount, $currency) {
     $maxDecimalPlaces = 0;
   }
   return number_format($amount, $maxDecimalPlaces) . ' ' . $currency;
+}
+
+function trimZeros($num) {
+  if (!is_numeric($num)) throw new InvalidArgumentException("Only numeric values accepted");
+  $numStr = (string) $num;
+  if (!contains($num, '.')) {
+    return $numStr;
+  } else {
+    return trim($num, '0.');
+  }
 }
