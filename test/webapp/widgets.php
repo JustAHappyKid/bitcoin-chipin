5f4baca159d2e3f6830211ec187dc75b2e721789
Chris Wagner <christopher.t.wagner@gmail.com>
2013-08-03 19:45:36 -0400

    test/webapp/widgets.php: Use newly-defined 'getWidget' helper-function to reduce duplication.

--- test/webapp/widgets.php
+++ test/webapp/widgets.php
@@ -10,21 +10,7 @@ use \Chipin\Widgets\Widget, \DateTime;
 class WidgetTests extends WebappTestingHarness {
 
   function testRenderingWidget() {
-    $u = getUser();
-    $w = new Widget;
-    $w->ownerID = $u->id;
-    $w->title = "Test Widget";
-    $w->ending = new DateTime('2020-06-30');
-    $w->goal = 100;
-    $w->currency = 'USD';
-    $w->raised = 30;
-//    $w->width = XXX?
-//    $w->height = XXX?
-//    $w->color = XXX?
-    $w->bitcoinAddress = $this->btcAddr();
-    $w->countryCode = 'CA';
-    $w->about = "This is a test widget!";
-    $w->save();
+    $w = getWidget();
     $this->get("/widgets/by-id/{$w->id}");
     $goalDiv = current($this->xpathQuery("//div[@class='goal']"));
     assertTrue(contains($goalDiv->textContent, '100.00 USD') ||
