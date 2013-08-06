029b8d1145dddfc37e8d785435aa07f0e0d22146
Chris Wagner <christopher.t.wagner@gmail.com>
2013-08-05 23:07:58 -0400

    test/mock/lib/spare-parts/web-client/http-simple.php: Add new hook as per change to freegeoip.net service.

--- test/mock/lib/spare-parts/web-client/http-simple.php
+++ test/mock/lib/spare-parts/web-client/http-simple.php
@@ -10,7 +10,11 @@ function get($url) {
   } else if (beginsWith($url, 'http://blockchain.info/tobtc?currency=USD')) {
     return '0.00778083';
   } else if ($url == 'http://api.easyjquery.com/ips/?ip=99.99.99.99') {
-    return '{"Country": "CH"}';
+    return '{"Country": "US"}';
+  } else if ($url == 'https://freegeoip.net/json/99.99.99.99') {
+    return '{"ip":"99.99.99.99","country_code":"US","country_name":"United States",' .
+      '"region_code":"","region_name":"","city":"","zipcode":"","latitude":38,' .
+      '"longitude":-97,"metro_code":"","areacode":""}';
   } else {
     throw new Exception("Mock 'get' function couldn't handle following URL: $url");
   }
