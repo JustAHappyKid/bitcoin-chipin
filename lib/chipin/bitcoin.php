b2382e51129563eaa029e547c431ec40f397a2cb
Chris Wagner <christopher.t.wagner@gmail.com>
2013-08-05 23:12:31 -0400

    lib/chipin/bitcoin.php: Remove commented-out 'getBalanceFromBlockchainDotInfo', and comment-out now-unused 'isValidAddress'.

--- lib/chipin/bitcoin.php
+++ lib/chipin/bitcoin.php
@@ -6,7 +6,7 @@ require_once 'spare-parts/database.php';
 require_once 'spare-parts/web-client/http-simple.php';  # HttpSimple\get
 require_once 'spare-parts/types.php';                   # isInteger
 
-use \Exception, \SpareParts\Database as DB; // \SpareParts\WebClient\HttpSimple as Http;
+use \Exception, \SpareParts\Database as DB;
 
 function getBalance($address, $currency = 'BTC') {
   $row = DB\selectExactlyOne('bitcoin_addresses', 'address = ?', array($address));
@@ -18,19 +18,6 @@ function getBalance($address, $currency = 'BTC') {
   */
   return $balanceWithPrecision;
 }
-
-/*
-function getBalanceFromBlockchainDotInfo($address, $currency = 'BTC') {
-  $result = Http\get('http://blockchain.info/q/addressbalance/' . $address);
-  if (!isInteger($result)) {
-    throw new InvalidAddress("$address appears to be an invalid Bitcoin address");
-  }
-  $btcBalance = intval($result) / 100000000;
-  $balanceWithPrecision = $currency == 'BTC' ? $btcBalance : fromBTC($btcBalance, $currency);
-  $balance = substr($balanceWithPrecision, 0, 4);
-  return $balance;
-}
-*/
 
 function toBTC($currency, $amount) {
   $perBTC = lastPriceForOneBTC($currency);
@@ -60,11 +47,11 @@ function fromBTC($amountInBTC, $currency) {
 }
 */
 
-//class InvalidAddress extends Exception {}
-
+/*
 function isValidAddress() {
   $result = Http\get('http://blockchain.info/q/addressbalance/' . $address);
   return isInteger($result);
 }
+*/
 
 function satoshisPerBTC() { return 100000000; }
