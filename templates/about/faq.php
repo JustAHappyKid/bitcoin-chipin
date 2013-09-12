c03e4d5c9cd3985ac1c5cc08072d2649e5337681
Chris Wagner <christopher.t.wagner@gmail.com>
2013-09-13 02:08:19 +0800

    templates/about/faq.php: Reorganize FAQ and add additional question (What is Bitcoin?) so the page doubles in purpose, allowing us to directly link to the FAQ from the "Powered by BitcoinChipin.com" / help link on the footer of each widget.

--- templates/about/faq.php
+++ templates/about/faq.php
@@ -8,10 +8,52 @@ class FaqPage extends AboutPageLayout {
     <div id="faq" class="faq_page">
       <div class="container">
         <?= $this->pageHeader('FAQ') ?>
+        <div style="font-size: 15px; margin-bottom: 25px;">
+          <p>BitcoinChipin.com is a free service to help you raise money for your cause
+            using Bitcoin. Learn more below.</p>
+        </div>
         <div class="row">
           <div class="span12">
             <div class="faq">
               <div class="number">1</div>
+              <div class="question">
+                What is Bitcoin?
+              </div>
+              <div class="answer">
+                Bitcoin is a revolutionary new kind of money and payment system. Bitcoin
+                solves a lot of problems with traditional currencies by allowing for easy and
+                cheap (usually free!) payments through a decentralized network with users all
+                over the world. It greatly lowers the bar for saving and sending money &mdash;
+                only Internet access is recquired!
+                To learn more about Bitcoin itself,
+                <a href="http://bitcoin.org">start with the video here</a>.
+              </div>
+            </div>
+            <div class="faq">
+              <div class="number">2</div>
+              <div class="question">
+                Where can I purchase bitcoins?
+              </div>
+              <div class="answer">
+                We recommend using
+                <a href="https://bitinstant.com/" target="_blank">BitInstant</a>.
+              </div>
+            </div>
+            <div class="faq">
+              <div class="number">3</div>
+              <div class="question">
+                What can I use BitcoinChipin.com for?
+              </div>
+              <div class="answer">
+                Just about anything &mdash; raise money to help keep your website running, for
+                charity, or just because you want to give it a try!
+                Since we never hold your bitcoins, we don't have any restrictions on what
+                you may use it for. We will soon be introducing a Widget Gallery and social-media
+                sharing to make it easier for you to promote your campaign.
+              </div>
+            </div>
+            <div class="faq">
+              <div class="number">4</div>
               <div class="question">
                 Are my bitcoins safe with BitcoinChipin.com?
               </div>
@@ -22,7 +64,7 @@ class FaqPage extends AboutPageLayout {
               </div>
             </div>
             <div class="faq">
-              <div class="number">2</div>
+              <div class="number">5</div>
               <div class="question">
                 What does this service cost?
               </div>
@@ -31,33 +73,12 @@ class FaqPage extends AboutPageLayout {
               </div>
             </div>
             <div class="faq">
-              <div class="number">3</div>
-              <div class="question">
-                What can I use BitcoinChipin.com for?
-              </div>
-              <div class="answer">
-                Since we never hold your bitcoins, we don't have any restrictions on what
-                you may use it for. We will soon be introducing a Widget Gallery and social-media
-                sharing to make it easier for you to promote your campaign.
-              </div>
-            </div>
-            <div class="faq">
-              <div class="number">4</div>
+              <div class="number">6</div>
               <div class="question">
                 Is there any restriction on the number of widgets I can create?
               </div>
               <div class="answer">
                 No, you may create as many widgets as you like.
-              </div>
-            </div>
-            <div class="faq">
-              <div class="number">5</div>
-              <div class="question">
-                Where can I purchase bitcoins?
-              </div>
-              <div class="answer">
-                We recommend using
-                <a href="https://bitinstant.com/" target="_blank">BitInstant</a>.
               </div>
             </div>
           </div>
