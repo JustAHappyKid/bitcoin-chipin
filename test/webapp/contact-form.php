5ffee02aff56d5a761c63fbc71a36fcc1247c55c
Chris Wagner <christopher.t.wagner@gmail.com>
2013-10-24 18:49:43 +0800

    test/webapp/contact-form.php: Add a number of tests over contact-form to assert it rejects certain cases of spam / attacks.

---
+++ test/webapp/contact-form.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Chipin\Test;
+
+require_once dirname(__FILE__) . '/harness.php';  # WebappTestingHarness
+
+use \SpareParts\Webapp\MaliciousRequestException;
+
+class ContactFormTests extends WebappTestingHarness {
+
+  function setUp() {
+    parent::setUp();
+    $this->createHomepageWidgets();
+  }
+
+  function testMaliciousContentIsRejected() {
+    $badStuff = array('../../../etc/passwd', "1';select pg_sleep(4); --",
+      ".././../.../.././windows/win.ini", "1some_inexistent_file_with_long_name%00.jpg",
+      "-1 or 94=94");
+    foreach ($badStuff as $b) {
+      $this->get('/');
+      try {
+        $this->submitFormExpectingErrors($this->getForm(),
+          array('name' => 'Fred', 'email' => 'fred@test.org', 'comments' => $b));
+      } catch (MaliciousRequestException $_) { /* We'll take that. */ }
+    }
+//    $this->get('/');
+//    try {
+//      $this->submitFormExpectingErrors($this->getForm(),
+//        array('name' => '"+response.write(9395793*9073153)+"', 'email' => 'jim@test.net',
+//              'comments' => "hi guys, what is the policy about posting nonsense?"));
+//    } catch (MaliciousRequestException $_) { /* We'll take that. */ }
+  }
+
+  function testThatCommentsMustBeOfReasonableLength() {
+    foreach (array('1', ')', 'hello') as $comments) {
+      $this->get('/');
+      $this->submitFormExpectingErrors($this->getForm(),
+        array('name' => 'John', 'email' => 'j@test.com', 'comments' => $comments));
+    }
+  }
+
+  function testAccessingContactUsPage() {
+    $this->get('/contact-us/');
+  }
+}
