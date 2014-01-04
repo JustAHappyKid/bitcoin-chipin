a7f7be59093204368ed2bf52a55cc2148f79707c
Chris Wagner <chris@easyweaze.net>
2014-01-04 16:01:02 -0500

    lib/chipin/blockchain-dot-info.php: Improve error-handling when checking result returned from Blockchain.info (upon balance lookup), attempting to distinguish from an error-messsage that would seem to indicate the provided Bitcoin addresss is entirely invalid from the use-case whereby there was simply some kind of network issue or Blockchain.info is temporarily misbehaving (e.g., returning empty strings).

--- lib/chipin/blockchain-dot-info.php
+++ lib/chipin/blockchain-dot-info.php
@@ -4,14 +4,22 @@ namespace Chipin\BlockchainDotInfo;
 
 require_once 'spare-parts/web-client/http-simple.php';  # HttpSimple\get
 require_once 'spare-parts/types.php';                   # isInteger
+require_once 'spare-parts/string.php';                  # beginsWith
 require_once 'chipin/bitcoin.php';                      # InvalidAddress
 
 use \SpareParts\WebClient\HttpSimple as Http, \Chipin\Bitcoin\InvalidAddress;
 
 function getBalanceInSatoshis($address) {
   $result = Http\get('http://blockchain.info/q/addressbalance/' . $address);
-  if (!isInteger($result)) {
-    throw new InvalidAddress("$address appears to be an invalid Bitcoin address");
+  if (isInteger($result)) {
+    return intval($result);
+  } else {
+    $e = strtolower($result);
+    if ($e == 'checksum does not validate' || beginsWith($e, 'illegal character') ||
+        in_array($e, array('input to short', 'input too short')))
+      throw new InvalidAddress("$address appears to be an invalid Bitcoin address");
+    else
+      throw new \Exception("Unexpected result received from blockchain.info when " .
+        "attempting to get balance of address $address: $result");
   }
-  return intval($result);
 }
