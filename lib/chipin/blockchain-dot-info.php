955db9d105583bf51549b2bba57d1ad4ceca6feb
Chris Wagner <chris@easyweaze.net>
2014-01-07 22:47:45 -0500

    lib/chipin/blockchain-dot-info.php: Add new hook to detect case where we've received an HTTP response from CloudFlare indicating that it could not connect to Blockchain's server(s), raising a more appropriate exception when that is detected.

--- lib/chipin/blockchain-dot-info.php
+++ lib/chipin/blockchain-dot-info.php
@@ -7,7 +7,8 @@ require_once 'spare-parts/types.php';                   # isInteger
 require_once 'spare-parts/string.php';                  # beginsWith
 require_once 'chipin/bitcoin.php';                      # InvalidAddress
 
-use \SpareParts\WebClient\HttpSimple as Http, \Chipin\Bitcoin\InvalidAddress;
+use \SpareParts\WebClient\HttpSimple as Http, \SpareParts\WebClient\HttpConnectionError,
+  \Chipin\Bitcoin\InvalidAddress;
 
 function getBalanceInSatoshis($address) {
   $result = Http\get('http://blockchain.info/q/addressbalance/' . $address);
@@ -16,10 +17,15 @@ function getBalanceInSatoshis($address) {
   } else {
     $e = strtolower($result);
     if ($e == 'checksum does not validate' || beginsWith($e, 'illegal character') ||
-        in_array($e, array('input to short', 'input too short')))
+        in_array($e, array('input to short', 'input too short'))) {
       throw new InvalidAddress("$address appears to be an invalid Bitcoin address");
-    else
+    } else if (contains($e, '<title>blockchain.info | 522: Connection timed out</title>') &&
+               contains($e, 'CloudFlare')) {
+      throw new HttpConnectionError("Connection to blockchain.info timed out " .
+                                    "(reported by CloudFlare)");
+    } else {
       throw new \Exception("Unexpected result received from blockchain.info when " .
         "attempting to get balance of address $address: $result");
+    }
   }
 }
