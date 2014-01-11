39a736203f156defc389a961d82028f7df06b08b
Chris Wagner <chris@easyweaze.net>
2014-01-11 15:09:42 -0500

    lib/chipin/webapp/routes.php: Comment-out unused route 'addressBalance'.

--- lib/chipin/webapp/routes.php
+++ lib/chipin/webapp/routes.php
@@ -5,7 +5,7 @@ namespace Chipin\WebFramework\Routes;
 require_once 'chipin/widgets.php';
 use \Chipin\Widgets\Widget;
 
-function addressBalance($a = null) { return '/bitcoin/address-balance/' . $a; }
+//function addressBalance($a = null) { return '/bitcoin/address-balance/' . $a; }
 function validAddress($a = null) { return '/bitcoin/valid-address/' . $a; }
 function checkWidgetProgress(Widget $widget) { return "/widgets/progress/{$widget->id}"; }
 function amountRaised(Widget $widget) { return "/widgets/amount-raised/{$widget->id}"; }
